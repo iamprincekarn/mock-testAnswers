f56e07c62aaccec3cd7560c0bcadb799eab64d87
iamprincekarn <84575785+iamprincekarn@users.noreply.github.com>
2022-09-08 14:12:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,270 @@
+let map1 = new Map();
+// map1.set("only for test purpose","test cleared");
+
+//Aptitude
+map1.set("Find the remainder when 1! + 2! + 3! +4! + 5! + ————– 1000! is divided by 8", "1");
+map1.set("Find the remainder when 1! + 2! + 3! +4! + 5! + ————– 1000! is divided by 14", "5");
+map1.set("Find the remainder of the expression of 12899 x 96 x 997 when divided by 5", "3");
+map1.set("Find the remainder when 7 7 7 7 7 7 7 – – – – – 43 times is divided by 13", "7");
+map1.set("25 is divided by 26, the remainder is?", "25");
+map1.set("Find the remainder when 12345678910...99100 is divided by 16?", "12");
+map1.set("What is the remainder when 123456789101112131415161718192021222324252627282930313233343536373839404142434481 is divided by 45?", "36");
+map1.set("What is the remainder when 3^21 + 9^21 + 27^21 + 81^21 is divided by (3^20+1)?", "60");
+map1.set("What is the remainder when 15^2010 +16^2011 is divided by 7?", "3");
+map1.set("What is the remainder when 30^40 is divided by 7?", "2");
+map1.set("What is the remainder when 22003 is divided by 17?", "8");
+map1.set("What is the remainder when (16^27+37) is divided by 17?", "2");
+map1.set("What is the remainder when 7^121 is divided by 17?", "10");
+map1.set("What is the remainder when 17^200 is divided by 18?", "1");
+map1.set("What is the remainder when 1281000 is divided by 153?", "52");
+map1.set("What is the remainder when 21000 is divided by 59?", "41");
+map1.set("What is the remainder when 17432 is divided by 109?", "0");
+map1.set("What is the remainder of 121121 divided by 144?", "121");
+map1.set("What is the remainder when 39^198 divided by 12", "18");
+map1.set("What is the remainder when 3^164 is divided by 162?", "81");
+map1.set("What is the remainder when ((32)32)32 is divided by 7?", "4");
+map1.set("What is the remainder when 97! is divided by 101?", "17");
+map1.set("When a number x is divided by 33 it leaves a remainder 20. What would be the remainder when the same no. is divided by 11.", "9");
+map1.set("When a number x is divided by 60 it leaves a remainder 25. What would be the remainder when the same no. is divided by 12.", "1");
+map1.set("When a number x is divided by 1331 it leaves a remainder 131. What would be the remainder when the same no. is divided by 121?", "10");
+map1.set("When a number p is divided by 16 it leaves a remainder 5. And when a number q is divided by 16 it leaves a remainder 3. What would be the remainder when (p+q) is divide by 16?", "8");
+map1.set("When a number p is divided by 16 it leaves a remainder 5. And when a number q is divided by 16 it leaves a remainder 3. What would be the remainder when (p * q) is divide by 8.", "7");
+map1.set("When a number p is divided by 20 it leaves a remainder 13. And when a number q is divided by 20 it leaves a remainder 13. What would be the remainder when (p + q) is divide by 20.", "16");
+map1.set("A number being divided by 52 gives remainder 45. If the number is divided by 13, the remainder will be?", "6");
+map1.set("A number x when divided by 289 leaves 18 as the remainder. The same number when divided by 17 leaves y as a remainder. The value of y is", "1");
+map1.set("If 17200 is divided by 18, the remainder is ?", "1");
+map1.set("Find the remainder when 321 is divided by 5 is", "3");
+map1.set("When 231 divided by 5 the remainder is", "2");
+map1.set("When a number x is divided by 64 it leaves a remainder 25. What would be the remainder when the same no. is divided by 16?", "9");
+map1.set("When (719 + 2) is divided by 6, find the remainder.", "1");
+
+
+
+//Verbal ability
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice.
+
+They greet me cheerfully every morning.
+
+A. Every morning I was greeted cheerfully.
+B. I am greeted cheerfully by them every morning.
+C. I am being greeted cheerfully by them every morning.
+D. Cheerful greeting is done by them every morning to me.`, `B`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice.
+
+After driving professor Kumar to the museum she dropped him at his hotel.
+
+A. After being driven to the museum, Professor Kumar was dropped at his hotel.
+B. Professor Kumar was being driven dropped at his hotel.
+C. After she had driven Professor Kumar to the museum she had dropped him at his hotel.
+D. After she was driven Professor Kumar to the museum she had dropped him at his hotel.`,`A`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice.
+
+They have built a perfect dam across the river.
+A.	Across the river a perfect dam was built.
+B.	A perfect dam has been built by them across the river.
+C.	A perfect dam should have been built by them.
+D.	Across the river was a perfect dam.`,`B`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice.
+
+A child could not have done this mischief.
+A. This mischief could not be done by a child.
+B. This mischief could not been done by a child.
+C. This mischief could not have been done by a child.
+D. This mischief a child could not have been done.`,`C`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice.
+
+You should open the wine about three hours before you use it.
+A. Wine should be opened about three hours before use.
+B. Wine should be opened by you three hours before use.
+C. Wine should be opened about three hours before you use it.
+D. Wine should be opened about three hours before it is used.`,`D`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice.
+
+Let everyone be given a chance.
+A. Give everyone a chance.
+B. Give a chance for everyone.
+C. Let a chance be given to everyone.
+D. I will give up a chance.`,`A`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice
+
+A detective arrested him as soon as he got home.
+A. An arrest was made by a detective as soon as he got home.
+B. He was arrested by a detective as soon as he got home.
+C. A detective was arrested by him as soon as he got home.
+D. An arrest was detected by him as soon as he got home.`,`C`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice
+
+They say he can teach almost anything in this world.
+A. It is said that almost anything in this world can be taught by him.
+B. It was said that almost anything could be taught by him.
+C. They say almost anything could be taught by him.
+D. They say anything almost will be taught by him.`,`A`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice
+
+The boys were digging a hole in the ground.
+A. A hole was being dug in the ground by the boys.
+B. In the ground, the boys dug a hole.
+C. A hole in the ground has been dug by the boys.
+D. A hole in the ground has been dug by the boys.`,`A`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice
+
+Their dog bit our neighbour yesterday.
+A. Our neighbour bit their dog yesterday.
+B. Our neighbour bitten by their do yesterday.
+C. Our neighbour was bitten by their dog yesterday.
+D. Our neighbour were bit by their dog yesterday.`,`C`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice
+
+Paint the windows.
+A.Windows should be painted.
+B.Let the windows be painted.
+C.Let be the windows painted.
+D.Windows are let to be painted.`,`B`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice
+
+I give alms to the poor on Saturdays.
+A. Alms are given to the poor by me on Saturdays.
+B. Alms is given to the poor by me on Saturdays.
+C. Alms are given to the poors by me on Saturdays.
+D. Alms were given to the poor by me on Saturdays.`,`A`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice
+
+If you cannot change the master, you cannot change the destiny.
+A. The destiny cannot be changed if masters cannot change.
+B. The destiny could not be changed if masters cannot be changed.
+C. The destiny cannot change if masters cannot be changed.
+D. The destiny cannot be changed if masters cannot be changed.`,`D`);
+
+map1.set(`Directions: Spot the Error
+
+(a) The teacher asked / (b) the students whether they could/ (c) tell the name of
+the man who had been invented steam engine. / (d) No error.`,`c`);
+
+map1.set(`Directions: Spot the Error
+
+(a) When the minister was shot dead,/ (b) people were run here and there / (c) to
+save themselves. /(d) No error.`,`b`);
+
+map1.set(`Directions: Spot the Error
+
+(a) If motorists do not observe the traffic regulations, (b) they will be stopped, ticketed / (c) and have to pay a fine. / (d) No error`,`c`);
+
+map1.set(`Directions: Spot the Error
+
+(a) The news of his father's death / (b) was not declared / (c) so far. / (d) No error`,`b`);
+
+map1.set(`Directions: Spot the Error
+
+(a) When the thief broke into their house / (b) they raised a hue and cry / (c) and the
+thief caught immediately by the people. / (d) No error`,`c`);
+
+map1.set(`Directions: Spot the Error
+
+(a) It was suggested / (b) by him that the baby / (c) should be immediately taken
+care. / (d) No error.`,`c`);
+
+map1.set(`Directions: Spot the Error
+
+(a) The truck driver was accused / (b) the lady for walking / (c) in the middle of the
+road./(d) No error.`,`a`);
+
+map1.set(`Directions: Spot the Error
+
+(a) I am extremely annoyed / (b) by/ (c) the mismanagement./(d) No error.`,`b`);
+
+map1.set(`Directions: Spot the Error
+
+(a) By the time he arrived, / (b) everybody had / (c) been gone./(d) No error.`,`c`);
+
+map1.set(`Directions: Spot the Error
+
+(a) Mother's milk / (b) cannot substituted by / (c) any thing else./(d) No error.`,`b`);
+
+map1.set(`Directions: Spot the Error
+
+(a) You behave in such / (b) a weird manner that / (c) everybody disgusted with you./
+(d) No error.`,`c`);
+
+map1.set(`Directions: Spot the Error
+
+(a) Ten mangoes / (b) are contained / (c) by this box./(d) No error.`,`c`);
+
+map1.set(`Directions: Spot the Error
+
+(a) The streets of Rome / (b) were thronged/ (c) by the common people./(d) No error.`,`c`);
+map1.set(`Directions: Spot the Error
+
+(a)The ship sank / (b) and everyone / (c) aboard drowned./(d) No error.`,`c`);
+map1.set(`Directions: Spot the Error
+
+(a) There will be no more supplies/ (b) unless all arrears of payment / (c) were
+cleared by next Monday. / (d) No error`,`c`);
+map1.set(`Directions: Spot the Error
+
+(a) Had the police not/ (b) reached here / (c) on time,the traders would have robbed./ (d) No error`,`c`);
+
+map1.set(`Directions: A sentence has been given in Active/Passive Voice. Out of the four
+alternatives suggested below, select the one which best expresses the same sentence
+in Passive/Active Voice.
+
+After taking her to the hospital, I dropped her at her place.
+A. After being taken to the hospital, she was dropped at her place by me.
+B. After been taken to the hospital, she was dropped by her place by me
+C. After being taken she was sent to hospital and dropped at her place by me.
+D. After being taken to the hospital, she was dropped on her place by me`,`A`);
+
+
+
+
+
+let x = document.getElementById("tArea");
+x.addEventListener('input', function () {
+    let str = this.value;
+    str = str.trim();
+    let ans = document.getElementById("answer");
+    if (str != "")
+        ans.innerHTML = map1.get(str);
+    else
+        ans.innerHTML = "";
+});
+
+
+
